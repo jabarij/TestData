16877d6bb7952b41dfbdc65564d051cb54b542e2
KOTRAK\bartlomiej.soter <bartlomiej.soter@kotrak.com>
2019-02-15 19:01:56 +0100

    - Implemented fixed instance factory; - Developed unit tests for fixed instance factory;

---
+++ src/TestData.Tests/Building/Standard/FixedInstanceFactoryTests.Constructor.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace TestData.Building.Standard
+{
+    partial class FixedInstanceFactoryTests
+    {
+        public class Constructor : FixedInstanceFactoryTests
+        {
+            [Fact]
+            public void NullInstance_ShouldThrow()
+            {
+                // arrange
+                // act
+                Action create = () => new FixedInstanceFactory<SomeClass>(null);
+
+                // assert
+                create.Should().Throw<ArgumentNullException>();
+            }
+
+            [Fact]
+            public void Instance_ShouldCreateWithGivenObject()
+            {
+                // arrange
+                var expected = new SomeClass();
+
+                // act
+                var sut = new FixedInstanceFactory<SomeClass>(expected);
+
+                // assert
+                sut.Instance.Should().BeSameAs(expected);
+            }
+
+            class SomeClass
+            {
+            }
+        }
+    }
+}
---
+++ src/TestData/Building/Standard/FixedInstanceFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestData.Building.Standard
+{
+    public class FixedInstanceFactory<T> : IInstanceFactory<T>
+        where T : class
+    {
+        public FixedInstanceFactory(T instance)
+        {
+            Instance = instance ?? throw new ArgumentNullException(nameof(instance));
+        }
+
+        public T Instance { get; }
+
+        public T Create(IEnumerable<INamedPropertyOverwriter> overwriters) => Instance;
+    }
+}

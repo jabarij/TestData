87fadeae5d4d45baa088ef4d0be984aafb30c2be
KOTRAK\bartlomiej.soter <bartlomiej.soter@kotrak.com>
2019-02-21 11:46:05 +0100

    - Implemented DynamicBuilder's extension method WithMany; - Developed unit tests for DynamicBuilder's extension method WithMany;

--- src/TestData/Building/Dynamic/DynamicBuilderExtensions.cs
+++ src/TestData/Building/Dynamic/DynamicBuilderExtensions.cs
@@ -16,6 +16,14 @@ namespace TestData.Building.Dynamic
         public static IDynamicBuilder<T> WithEmpty<T>(this IDynamicBuilder<T> builder, Expression<Func<T, string>> property) =>
             WithValue(builder, property, string.Empty);
 
+        public static IDynamicBuilder<T> WithMany<T, TElement>(this IDynamicBuilder<T> builder, Expression<Func<T, IEnumerable<TElement>>> property, int count, Func<int, TElement> elementFactory)
+        {
+            if (count < 1) throw new ArgumentOutOfRangeException(nameof(count), count, "Elements count must be greater than zero.");
+            if (elementFactory == null) throw new ArgumentNullException(nameof(elementFactory));
+            var elements = Enumerable.Range(0, count).Select(elementFactory);
+            return WithValue(builder, property, elements);
+        }
+
         public static IDynamicBuilder<T> WithDefault<T, TProperty>(this IDynamicBuilder<T> builder, Expression<Func<T, TProperty>> property) =>
             WithValue(builder, property, default(TProperty));
 
